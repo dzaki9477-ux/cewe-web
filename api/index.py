41b1f4de15c1cb96cbdd3ea9aae01237e300f099
dzaki9477-ux <dzaki9477@gmail.com>
2026-03-02 02:20:53 +0700

    Create api/index.py file

---
+++ api/index.py
@@ -0,0 +1,10 @@
+# Contents of index.py
+# Put your Python code here
+
+# Example function
+
+def example_function():
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    example_function()
